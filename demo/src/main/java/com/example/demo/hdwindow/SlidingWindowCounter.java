70c8f81a7ca320fde35a8bb0ee96f84f1aaa17af
chenxiao <chenxiao@lizhi.com>
2019-05-27 11:41:26 +0800

    1111

---
+++ demo/src/main/java/com/example/demo/hdwindow/SlidingWindowCounter.java
@@ -0,0 +1,54 @@
+package com.example.demo.hdwindow;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created in 2019-05-27 10:42.
+ *
+ * @author chenxiao
+ */
+public class SlidingWindowCounter {
+    private volatile SlotBaseCounter slotBaseCounter;
+    private volatile int windowSize;
+    private volatile int head;
+
+    /**
+     * 滑动
+     * @param windowSize
+     */
+    public SlidingWindowCounter(int windowSize) {
+        resizeWindow(windowSize);
+    }
+
+    public synchronized void resizeWindow(int windowSize) {
+        this.windowSize = windowSize;
+        this.slotBaseCounter = new SlotBaseCounter(windowSize);
+        this.head = 0;
+    }
+
+    public void increase() {
+        slotBaseCounter.increaseSlot(head);
+    }
+
+    public int totalAndAdvance() {
+        int total = totalCount();
+        advance();
+        return total;
+    }
+
+    public void advance() {
+        int tail = (head + 1) % windowSize;
+        slotBaseCounter.wipeSlot(tail);
+        head = tail;
+    }
+
+    public int totalCount() {
+        return slotBaseCounter.totalCount();
+    }
+
+    @Override
+    public String toString() {
+        return "total = " + totalCount() + " head = " + head + " >> " + slotBaseCounter;
+    }
+}
