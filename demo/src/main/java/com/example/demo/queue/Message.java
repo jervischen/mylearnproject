76d0b98aeb84bb746341f05af86ba256e7519fcf
chenxiao <chenxiao>
2019-10-10 17:42:21 +0800

    延时队列

--- demo/src/main/java/com/example/demo/queue/Message.java
+++ demo/src/main/java/com/example/demo/queue/Message.java
@@ -19,23 +19,18 @@ public class Message implements Delayed {
     private long excuteTime;// 延迟时长，这个是必须的属性因为要按照这个判断延时时长。
 
 
-    /**
-     * // 延迟任务是否到时就是按照这个方法判断如果返回的是负数则说明到期否则还没到期
-     * @param unit
-     * @return
-     */
+
+    // 自定义实现比较方法返回 1 0 -1三个参数
     @Override
-    public long getDelay(TimeUnit unit) {
-        return 0;
+    public int compareTo(Delayed delayed) {
+        Message msg = (Message) delayed;
+        return Integer.valueOf(this.id) > Integer.valueOf(msg.id) ? 1
+                : (Integer.valueOf(this.id) < Integer.valueOf(msg.id) ? -1 : 0);
     }
 
-    /**
-     * 自定义实现比较方法返回 1 0 -1三个参数
-     * @param o
-     * @return
-     */
+    // 延迟任务是否到时就是按照这个方法判断如果返回的是负数则说明到期否则还没到期
     @Override
-    public int compareTo(Delayed o) {
-        return 0;
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(this.excuteTime - System.nanoTime(), TimeUnit.NANOSECONDS);
     }
 }
