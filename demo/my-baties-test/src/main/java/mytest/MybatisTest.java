ac24dc5a565f893f81d0d55896d0620c14de14aa
chenxiao <chenxiao>
2019-11-20 20:54:59 +0800

    mybaties测试

---
+++ demo/my-baties-test/src/main/java/mytest/MybatisTest.java
@@ -0,0 +1,35 @@
+package mytest;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import pojo.User;
+
+import java.io.InputStream;
+
+/**
+ * @author Chen Xiao
+ * @since 2019-11-19 17:10
+ */
+public class MybatisTest {
+    public static void main(String[] args) throws Exception {
+        // 指定全局配置文件
+        String resource = "mybatis-config.xml";
+        // 读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 构建sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        // 获取sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            // 操作CRUD，第一个参数：指定statement，规则：命名空间+“.”+statementId
+            // 第二个参数：指定传入sql的参数：这里是用户id
+            User user = sqlSession.selectOne("MyMapper.selectUser", 1);
+            System.out.println(user);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+}
---
+++ demo/my-baties-test/src/main/java/pojo/User.java
@@ -0,0 +1,17 @@
+package pojo;
+
+import lombok.Data;
+
+/**
+ * @author Chen Xiao
+ *
+ * @since 2019-11-19 17:08
+ */
+@Data
+public class User {
+
+    private String id;
+    private String name;
+    private int age;
+
+}
