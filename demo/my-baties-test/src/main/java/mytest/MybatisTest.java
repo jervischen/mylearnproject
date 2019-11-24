e91bcdffd783dac2cbc44ed3b3a853b3024fcb88
chenxiao <chenxiao>
2019-11-24 11:44:18 +0800

    mybaties测试

--- demo/my-baties-test/src/main/java/mytest/MybatisTest.java
+++ demo/my-baties-test/src/main/java/mytest/MybatisTest.java
@@ -1,12 +1,20 @@
 package mytest;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 import pojo.User;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Chen Xiao
@@ -30,6 +38,43 @@ public class MybatisTest {
         } finally {
             sqlSession.close();
         }
+
+        System.out.println("Integer: " + ObjectSizeCalculator.getObjectSize(Integer.valueOf(1)));
+        System.out.println("Long: " + ObjectSizeCalculator.getObjectSize(Long.valueOf(1L)));
+
+
+        String a = "名";
+        System.out.println("UTF-8编码长度:"+a.getBytes("UTF-8").length);
+        System.out.println("GBK编码长度:"+a.getBytes("GBK").length);
+        System.out.println("GB2312编码长度:"+a.getBytes("GB2312").length);
+        System.out.println("==========================================");
+    }
+
+    @Test
+    public void testSort(){
+        User user1 = new User();
+        user1.setId(1);
+        user1.setAge(1);
+        user1.setName("a");
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setAge(10);
+        user2.setName("b");
+
+        List<User> list = Lists.newArrayList();
+        list.add(user1);
+        list.add(user2);
+
+        System.out.println(list);
+        Collections.sort(list);
+
+        System.out.println(list);
+
+        Map<String,String> a = Maps.newHashMap();
+        System.out.println(a.size());
+        List<String> ll = new ArrayList<>(a.size());
+        System.out.println(ll.isEmpty());
     }
 
 }
--- demo/my-baties-test/src/main/java/pojo/User.java
+++ demo/my-baties-test/src/main/java/pojo/User.java
@@ -8,10 +8,27 @@ import lombok.Data;
  * @since 2019-11-19 17:08
  */
 @Data
-public class User {
+public class User implements Comparable<User>{
 
-    private String id;
+    private long id;
     private String name;
     private int age;
 
+    @Override
+    public int compareTo(User o) {
+        if (this.age == o.getAge()){
+            return 0;
+        }
+
+        if (this.age > o.getAge()){
+            return -1;
+        }
+
+
+        if (this.age < o.getAge()){
+            return 1;
+        }
+
+        return 0;
+    }
 }
